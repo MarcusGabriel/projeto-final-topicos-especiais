e99d2a40b0f93861cc308db3dfaf717d9197d420
Marcus <marcus.gbrl@live.com>
2017-03-19 15:17:42 -0400

    commit inicial do projeto final de topicos especiais

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Projeto Final Tópicos</title>
+        
+        <link href='public/assets/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="public/assets/css/body.css">
+  
+    </head>
+
+    <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">                
+                <a class="navbar-brand" href="#">Tópicos Especiais em CCP</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+        <script src='public/assets/jquery/dist/jquery.js'></script>
+        <script src='public/assets/bootstrap/dist/js/bootstrap.min.js'></script>        
+    </body>
+</html>
+
+
